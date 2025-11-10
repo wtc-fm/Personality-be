ead9eae6eae97fe39f1af2c9c8c86d5ae118631e
starday119 <jy8294@naver.com>
2025-11-10 11:58:42 +0900

    ［init][jjaeroong]:프로젝트 초기 세팅

---
+++ src/main/kotlin/com/personalitytest/personalitytest/PersonalitytestApplication.kt
@@ -0,0 +1,11 @@
+package com.personalitytest.personalitytest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalitytestApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonalitytestApplication>(*args)
+}
---
+++ src/test/kotlin/com/personalitytest/personalitytest/PersonalitytestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personalitytest.personalitytest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonalitytestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
